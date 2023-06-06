e0a2c49c0bde16e201a473f8fae93569c568d02a
Bhuwan Paudel <bhuwanpaudel@hotmail.com>
2023-06-06 22:22:11 +0200

    Want to see the diff

---
+++ test.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
+class test{
+	public static void main(String[] args){
+		System.out.ptintln("Java programs")
+	}
+}
