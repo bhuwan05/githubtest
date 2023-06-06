aef195b357b580dfd9051c95693942dd08dd5d60
bhuwan05 <33065707+bhuwan05@users.noreply.github.com>
2023-06-06 22:57:23 +0200

    Add files via upload

---
+++ test.java
@@ -0,0 +1,41 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
+class test{
+	public static void main(String[] args){
+		System.out.ptintln("Java programs")
+	}
+}
+
+class test{
+	public static void main(String[] args){
+		System.out.ptintln("Git Lesson")
+	}
+}
+public class Main {
+
+  public static void main(String[] args) {
+
+    int num = 29;
+    boolean flag = false;
+    for (int i = 2; i <= num / 2; ++i) {
+      // condition for nonprime number
+      if (num % i == 0) {
+        flag = true;
+        break;
+      }
+    }
+
+    if (!flag)
+      System.out.println(num + " is a prime number.");
+    else
+      System.out.println(num + " is not a prime number.");
+  }
+}
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Final, test"); 
+    }
+}
